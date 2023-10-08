be7b235877daea144352f2da93a614f4ddea42f1
FAIZAN AHMED <108749861+FURIOUSCHAMP007@users.noreply.github.com>
2023-10-08 09:58:59 +0530

    Add files via upload

---
+++ M1/LABSHEET 1/Q1/TestThread.java
@@ -0,0 +1,16 @@
+class Task implements Runnable { 
+ public void run() {
+ System.out.println(Thread.currentThread()+" printing "); 
+ System.out.println("Welcome"); 
+ } 
+}
+ public class TestThread { 
+public static void main(String[] args) { 
+ Task task = new Task();
+ Thread t1= new Thread(task); 
+ t1.setName("first");
+ t1.start(); 
+System.out.println(Thread.currentThread()+" printing ");
+System.out.println("to Java"); 
+ }
+}
